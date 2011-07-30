6a7fbd5bda27ae076976c79f3a3377ad5665721e
540401582@qq.com <540401582@qq.com>
2011-07-30 10:28:11 +0800

    have a try again

---
+++ qi/MyFirstProgram.java
@@ -0,0 +1,12 @@
+public class MyFirstProgram
+{
+    public static void main(String[] args)
+    {
+            System.out.println("Hello,how are you?");
+            try
+            {
+                       System.in.read();
+            }
+           catch(Exception e){}
+     }
+}
