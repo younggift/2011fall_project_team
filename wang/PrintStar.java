5b8361428bcdc2a596a1039071bb801e71b73766
wangtuizhi <wangq055@sina.com>
2011-08-05 22:03:42 +0800

    wang/PrintStar.java

---
+++ wang/PrintStar.java
@@ -0,0 +1,22 @@
+
+public class test01 {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int j;
+		for(i=0;i<10;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				if(j<i)
+				System.out.print("*");
+				else
+					System.out.println("*");
+			}
+				
+			}
+			
+	}
+
+}
