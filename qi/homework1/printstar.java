709f21ee377ba38b1c1ba399a23b21c855aab571
540401582@qq.com <540401582@qq.com>
2011-08-10 15:27:19 +0800

    the fifth week

--- qi/homework1/printstar.java
+++ qi/homework1/printstar.java
@@ -1,15 +1,17 @@
+import java.util.Scanner;
+
 
 public class Print {
 
 
 	public static void main(String[] args) {
-		int k=1;
-		for(int j=0;j<20;j++){
-			for(int i=0;i<k;i++){
+		Scanner input = new Scanner(System.in);
+		int m =  input.nextInt(); 
+		for(int j=0;j<=m;j++){
+			for(int i=0;i<j;i++){
 			    System.out.print("*");
 			   }
 			   System.out.println(" ");
-			   k++;
 			  }
 
 
