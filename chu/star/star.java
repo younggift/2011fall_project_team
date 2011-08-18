363b3574419e843223e0e03a1eaa902026aa2052
chu bingqian <643959815@qq.com>
2011-08-18 11:34:13 +0800

    chu

--- chu/star/star.java
+++ chu/star/star.java
@@ -1,7 +1,7 @@
 public class star {
 	public static void main(String[] args) {	
 		
-		for(int j=1;j<=3;j++){			
+		for(int j=1;j<=4;j++){			
 			for(int i=0;i<j;i++){			  
 				System.out.print("*");			 
 				}		
