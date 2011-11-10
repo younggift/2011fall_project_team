f071b09427a16f013fc39b2404ed0210f6eae7c0
zerinhe <zerin.he@gmail.com>
2011-11-10 19:09:57 +0800

    he

--- he/Mine_Clearance/code/Mine_Clearance.java
+++ he/Mine_Clearance/code/Mine_Clearance.java
@@ -7,7 +7,9 @@ class  Mine_Clearance
 {
 	public static void main(String[] args) 
 	{
-		new PlayGame().Play();
+		//new PlayGame().Play();
+		PlayGame playgame = new PlayGame();
+		playgame.Play();
 	}
 }
 
@@ -33,6 +35,7 @@ class Mine
 		//若不加this关键字,可能会报错。提示如：找不到符号等
 		this.mine = new int[row][line];
 		this.mine2 = new char[row][line];
+		Initialize();
 	}
 	//构造方法结束
 
@@ -52,59 +55,70 @@ class Mine
 		//初始化雷
 		{
 			int i = 0,j = 0;
+			for (i = 0;i < row;i++)
+			{
+				for(j = 0;j<line;j++)
+				{this.mine[i][j] = 0;}
+			}
 			int sum = 0;
 			while(sum < 10)
 			{
 				//通过Random方法确定雷的位置
-				//【http://wenku.baidu.com/view/c8376309f78a6529647d53cb.html】说的i = (int)Math.Random()*10;无法通过编译
 				//下面的语句可以
-				i = (int)Math.random()*10;
-				j = (int)Math.random()*10;
+				i = (int)(Math.random()*10);
+				j = (int)(Math.random()*10);
 				//先判断该处是否已经有雷，若无这步可能最终雷的数量比10少
-				if(mine[i][j]==-1) continue;
-				else mine[i][j] = -1;
-				sum++;
+				if(this.mine[i][j]==-1) continue;
+				//else
+					this.mine[i][j] = -1;
+					sum++;
 				//以下8个判断语句用来统计某一位置周围雷的个数
 				//每产生一个雷就让周围八个位置计数，进行++操作
-				if(!this.Over(i-1,j))
+				if((this.mine[i-1][j]!=-1)&&!(this.Over(i-1,j)))
 				{
 					this.mine[i-1][j]++;
 				}
-				if(!this.Over(i+1,j))
+				if((this.mine[i+1][j]!=-1)&&!(this.Over(i+1,j)))
 				{
 					this.mine[i+1][j]++;
 				}
-				if(!this.Over(i,j+1))
+				if((this.mine[i][j+1]!=-1)&&!(this.Over(i,j+1)))
 				{
 					this.mine[i][j+1]++;
 				}
-				if(!this.Over(i,j-1))
+				if((this.mine[i][j-1]!=-1)&&(!this.Over(i,j-1)))
 				{
 					this.mine[i][j-1]++;
 				}
-				if(!this.Over(i-1,j-1))
+				if((this.mine[i-1][j-1]!=-1)&&(!this.Over(i-1,j-1)))
 				{
 					this.mine[i-1][j-1]++;
 				}
-				if(!this.Over(i+1,j-1))
+				if((this.mine[i+1][j-1]!=-1)&&(!this.Over(i+1,j-1)))
 				{
 					this.mine[i+1][j-1]++;
 				}
-				if(!this.Over(i-1,j+1))
+				if((this.mine[i-1][j+1]!=-1)&&(!this.Over(i-1,j+1)))
 				{
 					this.mine[i-1][j+1]++;
 				}
-				if(!this.Over(i+1,j+1))
+				if((this.mine[i+1][j+1]!=-1)&&(!this.Over(i+1,j+1)))
 				{
 					this.mine[i+1][j+1]++;
-				}
+				}				
+			}
+			for(i = 0;i<row;i++)
+			{
+				for(j = 0;j<line;j++)
+				{this.mine2[i][j] = '#';}
+
 			}
 		}
 		//布雷方法结束
 
 //输出界面方法
 public void Print_limsurface(){
-		System.out.println("------------");  
+		System.out.println("------------------------");  
 		//为每次输出界面画个框
 		System.out.print(" ");
 		for(int j=0;j<line;j++) System.out.print(" "+j);
@@ -118,7 +132,7 @@ public void Print_limsurface(){
 			//打印出每一次挖雷操作后的界面；
 			System.out.println("");
 		}
-		System.out.println("------------"); 
+		System.out.println("------------------------"); 
 		//边框部分；
 	}
 	//输出界面方法结束
@@ -152,7 +166,7 @@ public void Print_limsurface(){
 			boolean status = this.Dig(x,y);
 			//用变量status来记录是否挖到雷
 			if(!status)
-			{break Item;}
+			{break;}
 			else
 			{
 				if(operate.equals("F"))
@@ -246,12 +260,38 @@ class PlayGame
 //构造函数
 	public PlayGame()
 	{
-		Mine mine = new Mine();
+		this.mine = new Mine();
 	}
+
+	public void Help()
+	{
+		System.out.println("同学你现在在帮助页面");
+		//System.out.println("游戏中共有10个雷，你可以进行的操作有 挖雷 插旗。");
+		//System.out.println("如想挖坐标为（3,4）位置，请输入W34.");
+		//System.out.println("如想在坐标为（3,4）位置插旗，请输入F34");
+
+	}
+
 
 	public void Play()
 	{
-		//Scanner sc = new Scanner(System.in);
-		this.mine.Begin();
+		int option = 0;
+
+		while(true)
+		{
+			System.out.println("请输入要进行的操作：0=帮助 1=开始游戏 2=退出游戏");
+		    Scanner sc = new Scanner(System.in);
+		    option = sc.nextInt();
+			if(option == 0)
+			{this.Help();}
+			else if(option == 1)
+			{this.mine.Begin();}
+			else if(option == 2)
+			{
+				System.out.println("你已退出游戏！");
+				System.exit(0);
+			}
+			else{System.out.println("Input Error!");}
+		}
 	}
 }
