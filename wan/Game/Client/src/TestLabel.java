18b871a00368588b72d0638834043d03bb3b4bf9
wanjiang <wanjiang1991@gmail.com>
2012-10-11 17:38:01 +0800

    modified（the Recent Version)  _by wanjiang
    Signed-off-by: wanjiang <wanjiang1991@gmail.com>

---
+++ wan/Game/Client/src/TestLabel.java
@@ -0,0 +1,10 @@
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Administrator
+ */
+class TestLabel extends JLabel{
+    boolean sign;
+    int number;
+}
---
+++ wan/Game/Server/compile.bat
@@ -0,0 +1,3 @@
+cd src
+javac IMServer.java
+move *.class classes
